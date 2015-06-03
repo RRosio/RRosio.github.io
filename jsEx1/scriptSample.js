c291e8a4bc4b5fbb1084c22d02520be3d0c8a3b9
RRosio <0632015@my.scccd.edu>
2015-06-02 21:12:23 -0700

    New

--- jsEx1/scriptSample.js
+++ jsEx1/scriptSample.js
@@ -1,6 +1,24 @@
-var myElement = document.createElement('img');
-myElement.src = 'http://lorempixel.com/120/120/sports/2/';
-myElement.alt = 'Sports Image 2';
+var myNode = document.querySelector('.item');
+var myImages = [
+	'http://lorempixel.com/output/city-q-c-640-480-7.jpg',
+	'http://lorempixel.com/output/animals-q-c-640-480-7.jpg',
+	'http://lorempixel.com/output/abstract-q-g-640-480-9.jpg',
+	'http://lorempixel.com/output/abstract-q-c-640-480-5.jpg',
+	'http://lorempixel.com/output/city-q-g-640-480-2.jpg',
+	'http://lorempixel.com/output/people-q-g-640-480-5.jpg',
+	'http://lorempixel.com/output/technics-q-g-640-480-4.jpg',
+	'http://lorempixel.com/output/cats-q-c-640-480-8.jpg',
+	'http://lorempixel.com/output/nature-q-c-640-480-1.jpg',
+];
 
-myNode.appendChild(myElement);
-myNode.appendChild(otherElement);
+for (var i = 0; i < myImages.length; i++)
+{
+	var myElement = document.createElement('img');
+	myElement.src = myImages[i];
+	myElement.alt = 'Images ' + i;
+	myElement.style.borderRadius = '10px';
+	myElement.className= 'bigger';
+	myNode.appendChild(myElement);
+}
+
+
---
+++ jsEx2/script.js
@@ -0,0 +1,29 @@
+//document.querySelector('.boxes').addEventListener('click',
+//   myFunction, false);
+document.querySelector('.boxes').addEventListener('click',
+function() {
+	var a = '';
+	e.target.id= a;
+
+	if (a=== '';1)
+  	{
+  		e.target.id='blue';
+  	}
+  	else
+  	{
+  		e.target.id='pink';
+  	} 
+}, false);
+
+/* function myFunction(e) {
+  if (e.target.id==='pink')
+  {
+  	e.target.id='blue';
+  }
+  else
+  {
+  	e.target.id='pink';
+  }
+}
+*/
+
